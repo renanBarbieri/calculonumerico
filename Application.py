5b9a2cca17bfdd13b7f11797dc66869df52091ca
Renan Barbieri <renan.hbarbieri@gmail.com>
2018-10-11 11:13:51 -0300

    definindo a função

--- Application.py
+++ Application.py
@@ -4,6 +4,10 @@ from NewtonRaphson import NewtonRaphson
 
 class Application(object):
 
+    cs = 9 # mg/l
+    c0 = 2 # mg/l
+    ka = 0.88 # /dia
+
     def __init__(self):
         print("Iniciando a aplicação")
         x0 = 30
@@ -12,12 +16,10 @@ class Application(object):
 
     def f(self, x):
         print "Getting fx at "+x
-        cs = 0
-        c0 = 0
-        ka = 0
-        return cs - ((cs - c0) * exp(ka * x))
+        return self.cs - ((self.cs - self.c0) * exp(self.ka * x))
 
     def dfdx(self, x):
-        print "Getting dfdx"
+        print "Getting dfdx at "+x
+        return exp(self.ka * x) * self.ka * (self.cs - self.c0)
 
 Application()
